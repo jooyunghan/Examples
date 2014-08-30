84218e353345d1af8d93cfd0f84fa9de059d72da
Jooyung Han <jooyung.han@gmail.com>
2014-08-30 09:06:43 +0900

    Move reversi() to Games

--- src/com/lge/boardgame/Games.java
+++ src/com/lge/boardgame/Games.java
@@ -6,26 +6,33 @@ import java.awt.*;
 
 public class Games {
 
-	public static BoardGamePanel tictactoe() {
-		int size = 3;
-		MakeLineGame game = new MakeLineGame(size, size, new HumanPlayer(),
-				new ComputerPlayer(new MinimaxAi()));
-		BoardDrawer drawer = new BoardDrawer(Drawers.grid(size),
-				Drawers.whiteO, Drawers.X);
-		return new BoardGamePanel(game, drawer);
-	}
+    public static BoardGamePanel tictactoe() {
+        int size = 3;
+        MakeLineGame game = new MakeLineGame(size, size, new HumanPlayer(),
+                new ComputerPlayer(new MinimaxAi()));
+        BoardDrawer drawer = new BoardDrawer(Drawers.grid(size),
+                Drawers.whiteO, Drawers.X);
+        return new BoardGamePanel(game, drawer);
+    }
 
-	public static BoardGamePanel omok() {
-		int size = 19;
-		MakeLineGame game = new MakeLineGame(size, 5,
-				new ComputerPlayer(new RandomAi()),
-				new ComputerPlayer(new RandomAi()),
-				new ComputerPlayer(new RandomAi()));
-//				new HumanPlayer(),
-//				new HumanPlayer());// new ComputerPlayer(new RandomAi()));
-		BoardDrawer drawer = new BoardDrawer(Drawers.gridOnCenter(size),
-				Drawers.whiteO, Drawers.blackO, Drawers.filledO(Color.green));
-		return new BoardGamePanel(game, drawer);
-	}
+    public static BoardGamePanel omok() {
+        int size = 19;
+        MakeLineGame game = new MakeLineGame(size, 5, new ComputerPlayer(
+                new RandomAi()), new ComputerPlayer(new RandomAi()),
+                new ComputerPlayer(new RandomAi()));
+        // new HumanPlayer(),
+        // new HumanPlayer());// new ComputerPlayer(new RandomAi()));
+        BoardDrawer drawer = new BoardDrawer(Drawers.gridOnCenter(size),
+                Drawers.whiteO, Drawers.blackO, Drawers.filledO(Color.green));
+        return new BoardGamePanel(game, drawer);
+    }
+
+    public static Component reversi(int size) {
+        BoardDrawer drawer = new BoardDrawer(Drawers.grid(size),
+                Drawers.whiteO, Drawers.blackO);
+        BoardGamePanel panel = new BoardGamePanel(new ReversiGame(size,
+                new HumanPlayer(), new ComputerPlayer(new RandomAi())), drawer);
+        return panel;
+    }
 
 }
--- src/com/lge/boardgame/Main.java
+++ src/com/lge/boardgame/Main.java
@@ -10,25 +10,19 @@ import com.lge.boardgame.ai.RandomAi;
 
 public class Main {
 
-	public static void main(String[] args) throws InvocationTargetException,
-			InterruptedException {
-		SwingUtilities.invokeAndWait(new Runnable() {
-			@Override
-			public void run() {
-				JFrame frame = new JFrame("BoardGame");
-				int size = 4;
-				BoardDrawer drawer = new BoardDrawer(Drawers.grid(size),
-						Drawers.whiteO, Drawers.blackO);
-				BoardGamePanel panel = new BoardGamePanel(new ReversiGame(size,
-						new HumanPlayer(), new ComputerPlayer(new RandomAi())),
-						drawer);
-				frame.add(panel);
-				frame.setSize(new Dimension(600, 600));
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setVisible(true);
-				// frame.setResizable(false);
-			}
-		});
-	}
+    public static void main(String[] args) throws InvocationTargetException,
+            InterruptedException {
+        SwingUtilities.invokeAndWait(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("BoardGame");
+                frame.add(Games.reversi(4));
+                frame.setSize(new Dimension(600, 600));
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+                // frame.setResizable(false);
+            }
+        });
+    }
 
 }
